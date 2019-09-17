2b8418b4ac34af70c16eb9f40870554c7cd45981
humbertopeluso <humbertopeluso@yahoo.com.br>
2019-09-17 20:40:30 -0300

    Obtendo usuário por Id

--- src/main/java/com/humberto/workshopmongodb/services/UserService.java
+++ src/main/java/com/humberto/workshopmongodb/services/UserService.java
@@ -2,10 +2,12 @@ package com.humberto.workshopmongodb.services;
 
 import com.humberto.workshopmongodb.domain.User;
 import com.humberto.workshopmongodb.repository.UserRepository;
+import com.humberto.workshopmongodb.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,6 +17,11 @@ public class UserService {
 
     public List<User> findAll() {
         return repo.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 
 }
