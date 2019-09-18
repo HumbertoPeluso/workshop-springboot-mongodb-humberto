54eb3440bce350211ea16472cd13d5aefae15df1
humbertopeluso <humbertopeluso@yahoo.com.br>
2019-09-18 15:21:55 -0300

    Endpoint para retornar os posts do usuário

--- src/main/java/com/humberto/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/humberto/workshopmongodb/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.humberto.workshopmongodb.resources;
 
+import com.humberto.workshopmongodb.domain.Post;
 import com.humberto.workshopmongodb.domain.User;
 import com.humberto.workshopmongodb.dto.UserDTO;
 import com.humberto.workshopmongodb.services.UserService;
@@ -54,6 +55,12 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 
 
